608010cd034c05a4552c8fadb39fee0bc7f72a67
Sen Morgan <technars@gmail.com>
2022-06-18 17:22:52 +0200

    Initial commit

---
+++ lib/defs/def.h
@@ -0,0 +1,25 @@
+/**
+ * @file def.h
+ * @author SenMorgan https://github.com/SenMorgan
+ * @date 2022-06-18
+ *
+ * @copyright Copyright (c) 2021 Sen Morgan
+ *
+ */
+
+#ifndef _DEF_H_
+#define _DEF_H_
+
+// MQTT definitions
+#define DEFAULT_TOPIC             "/inverter/"
+#define MQTT_WILL_TOPIC           DEFAULT_TOPIC "availability"
+#define MQTT_QOS                  1
+#define MQTT_RETAIN               0
+#define MQTT_WILL_MESSAGE         DEFAULT_TOPIC "offline"
+#define MQTT_SUBSCRIBE_TOPIC      DEFAULT_TOPIC "#"
+#define MQTT_PUBLISH_TOPIC        DEFAULT_TOPIC "state"
+#define MQTT_AVAILABILITY_TOPIC   DEFAULT_TOPIC "availability"
+#define MQTT_AVAILABILITY_MESSAGE "online"
+
+
+#endif // _DEF_H_

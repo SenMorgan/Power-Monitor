56791dbf5d8781c02cc101f9863e785fbd1d990f
Sen Morgan <technars@gmail.com>
2022-06-18 17:33:14 +0200

    feat: Add libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <INA226.h>
+#include <EEPROM.h>
 
 #include "credentials.h"
 #include "def.h"
